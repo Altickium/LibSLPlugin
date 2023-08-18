541fb78aee1ccaa8810bdb628ff54659709ee770
GitHub Action <action@github.com>
2023-08-18 20:53:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/altickium/libslplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/altickium/libslplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.altickium.libslplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
